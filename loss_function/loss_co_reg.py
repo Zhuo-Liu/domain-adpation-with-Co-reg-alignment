686449c86a8c9d34e542ee56291536a952956818
Zhuo Liu <zhuol@mit.edu>
2021-11-19 22:10:26 -0500

    Update loss_co_reg.py

--- loss_function/loss_co_reg.py
+++ loss_function/loss_co_reg.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 def diverse_loss(feat1, feat2, margin):
     loss = torch.sqrt(torch.sum((torch.mean(feat1 - feat2, 0)) ** 2))
     if loss > margin:
-        return margin
+        return 0
     else:
         return loss
 
